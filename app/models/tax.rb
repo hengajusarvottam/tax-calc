db9e517a04b53b9f31ea74fcf89210b574eac693
hengajusarvottam <sarvottam.hengaju@gurzu.com>
2023-10-27 15:02:07 +0545

    Calculate tax with  bonus and cit

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -3,6 +3,9 @@
 class Tax < ApplicationRecord
     # validates :monthly_income, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :assessment_type, presence: true
+    validates :yearly_bonus, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+    validates :annual_cit, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+
     ASSESSMENT_TYPES = ["Single", "Couple"]
 
   
